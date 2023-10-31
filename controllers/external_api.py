544e917a9e33693180c85f9558bade6eff706f2b
mahmoud <eng.barakat11@gmail.com>
2023-10-31 15:13:07 +0200

    2nd update

--- controllers/external_api.py
+++ controllers/external_api.py
@@ -10,33 +10,39 @@ common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
 uid = common.authenticate(db, username, password, {})
 
 if uid:
-    print("authentication succeeded")
+    # print("authentication succeeded")
 
     models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
     # search methods
-    partners = models.execute_kw(db, uid, password, 'res.partner', 'search', [[['is_company', '=', True]]],
-                                 {'limit': 5})
+    # partners = models.execute_kw(db, uid, password, 'res.partner', 'search', [[['is_company', '=', True]]],
+    #                              {'limit': 5})
+    #
+    # # search count
+    # partners_count = models.execute_kw(db, uid, password, 'res.partner', 'search_count', [[['is_company', '=', True]]])
+    # print("partners count = ", partners_count)
+    #
+    # # read methods
+    # partner_rec = models.execute_kw(db, uid, password, 'res.partner', 'read', [partners], {'fields': ['id', 'name']})
+    #
+    # # search read
+    # partner_rec2 = models.execute_kw(db, uid, password, 'res.partner', 'search_read', [[['is_company', '=', True]]], {'fields': ['id', 'name']})
+    # print(">>>>>>",partner_rec2)
+    #
+    #
+    # #create record
+    # vals = {
+    #     "name": "new external api",
+    #     "email": "newexternalapi@gmail"
+    # }
+    # created_id = models.execute_kw(db, uid, password, 'res.partner', 'create', [vals])
+    # print("created record >>", created_id)
 
-    # search count
-    partners_count = models.execute_kw(db, uid, password, 'res.partner', 'search_count', [[['is_company', '=', True]]])
-    print("partners count = ", partners_count)
 
-    # read methods
-    partner_rec = models.execute_kw(db, uid, password, 'res.partner', 'read', [partners], {'fields': ['id', 'name']})
+    #update record
+    models.execute_kw(db, uid, password, 'res.partner', 'write', [[70], {'phone': "010012"}])
 
-    # search read
-    partner_rec2 = models.execute_kw(db, uid, password, 'res.partner', 'search_read', [[['is_company', '=', True]]], {'fields': ['id', 'name']})
-    print(">>>>>>",partner_rec2)
-
-
-    #create record
-    vals = {
-        "name": "new external api",
-        "email": "newexternalapi@gmail"
-    }
-    created_id = models.execute_kw(db, uid, password, 'res.partner', 'create', [vals])
-    print("created record >>", created_id)
-
+    # delete record
+    models.execute_kw(db, uid, password, 'res.partner', 'unlink', [[71]])
 
 
 
